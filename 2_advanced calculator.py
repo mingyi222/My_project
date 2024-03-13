bab0835b8e7daa399ec66c388f4daf941992c7d5
mingyi222 <mike.mychou@gmail.com>
2024-03-13 10:51:50 +0800

    py + -

---
+++ 2_advanced calculator.py
@@ -0,0 +1,21 @@
+#
+# ouypuy test
+# Mingyi Chou
+#
+
+# 1. Input
+x1 = input('Enter x1: ')
+x2 = input('Enter x2: ')
+op = input('Enter operator: ')
+
+# 2. process
+
+if op == '+':
+    sum = int(x1) + int(x2)
+elif op == '-':
+    sum = int(x1) - int(x2)
+
+# 3. Output
+
+print(f'Sum: {sum}')
+
